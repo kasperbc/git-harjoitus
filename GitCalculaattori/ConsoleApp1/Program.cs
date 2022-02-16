a5302041678f8f69f53a8f0f0485c17dd652286f
kasper <s2000925@edu.bc.fi>
2022-02-16 12:04:13 +0200

    Lisättiin laskuvalinta

--- GitCalculaattori/ConsoleApp1/Program.cs
+++ GitCalculaattori/ConsoleApp1/Program.cs
@@ -8,10 +8,27 @@
         Console.WriteLine("Anna toinen numero:");
         int numero2 = int.Parse(Console.ReadLine());
 
-        
-        int summa = numero1 + numero2;
-        Console.WriteLine("Numeroiden summa on " + summa);
-        int erotus = numero1 - numero2;
-        Console.WriteLine("Numeroiden erotus on " + erotus);
+        while(true)
+        {
+            Console.WriteLine("Miten haluat laskea luvut? (summa, erotus)");
+            string laskuToiminto = Console.ReadLine();
+
+            if (laskuToiminto.Equals("summa"))
+            {
+                int summa = numero1 + numero2;
+                Console.WriteLine("Numeroiden summa on " + summa);
+                break;
+            }
+            else if (laskuToiminto.Equals("erotus"))
+            {
+                int erotus = numero1 - numero2;
+                Console.WriteLine("Numeroiden erotus on " + erotus);
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Väärä komento.");
+            }
+        }
     }
 }
